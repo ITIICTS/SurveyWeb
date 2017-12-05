796511b79cf0975afe6498ec96efd712937a2853
alanpebr <33680403+alanpebr@users.noreply.github.com>
2017-12-05 19:28:35 +0700

    Add BongkarContainerController.cs and ContainerDurationController.cs

---
+++ ITI.Survey.Web/ITI.Survey.Web.UI/Controllers/BongkarContainerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITI.Survey.Web.UI.Controllers
+{
+    [Authorize]
+    public class BongkarContainerController : BaseController
+    {
+        public ActionResult BongkarContainerAV()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProsesBongkarContainerAV()
+        {
+            using (var stackingService = new StackingWebService.StackingSoapClient()) 
+            {
+                //stackingService.
+            }
+            return Json(new { }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult BongkarContainerDM()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProsesBongkarContainerDM()
+        {
+            return Json(new { }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ITI.Survey.Web/ITI.Survey.Web.UI/Controllers/ContainerDurationController.cs
@@ -0,0 +1,67 @@
+﻿using AGY.Solution.Helper;
+using AGY.Solution.Helper.Common;
+using ITI.Survey.Web.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web.Mvc;
+
+namespace ITI.Survey.Web.UI.Controllers
+{
+    [Authorize]
+    public class ContainerDurationController : BaseController
+    {
+        // GET: ContainerDuration
+        public ActionResult Index()
+        {
+            ContainerDurationModel model = new ContainerDurationModel();
+            GetConfigXML();
+            return View(model);
+        }
+
+        IList<ContainerDurationModel> DataContainerDuration(string userId, string customerCode, string size, string type, string condition, int minDuration, string sortBy)
+        {
+            IList<ContainerDurationModel> result = new List<ContainerDurationModel>();
+
+            using (var stackingService = new StackingWebService.StackingSoapClient())
+            {
+                String xml = stackingService.FillContainerDuration(userId, customerCode, size, type, condition, minDuration, sortBy);
+                if (!string.IsNullOrEmpty(xml))
+                {
+                    DataSet ds = ConvertToDataTable.ConvertXmlToDataSet(xml);
+                    DataTable dt = ds.Tables[0];
+                    result = dt.ToList<ContainerDurationModel>();
+                }
+            }
+
+            return result;
+        }
+
+        [HttpPost]
+        public ActionResult DataTableContainerDuration(ContainerDurationModel objSearch)
+        {
+            DataTableModel objPage = new DataTableModel { CurrentPage = 1, IsDescending = true, PageSize = int.MaxValue, PropertyOrder = "Cont" };
+            IList<ContainerDurationModel> resultSearch = DataContainerDuration(Username, objSearch.CustomerCode, objSearch.Size, objSearch.Type, objSearch.Condition, objSearch.Duration, objPage.PropertyOrder);
+            objPage.TotalRow = resultSearch.Count;
+            ViewBag.Pagination = PageHelper.CreateLink(objPage.CurrentPage, objPage.TotalRow, objPage.PageSize, true);
+            return View("_DataTableContainerDuration", resultSearch);
+        }
+
+        [HttpPost]
+        public ActionResult DataTableContainerDurationJson(string stringSearch, int page = 1, int pageSize = int.MaxValue, string propertyOrder = "CreatedDate", bool isDescending = true)
+        {
+            ContainerDurationModel objSearch = Newtonsoft.Json.JsonConvert.DeserializeObject<ContainerDurationModel>(stringSearch, JSONSetting);
+            DataTableModel objPage = new DataTableModel { CurrentPage = page, IsDescending = isDescending, PageSize = pageSize, PropertyOrder = propertyOrder };
+            IList<ContainerDurationModel> resultSearch = DataContainerDuration(Username, objSearch.CustomerCode, objSearch.Size, objSearch.Type, objSearch.Condition, objSearch.Duration, objPage.PropertyOrder);
+            objPage.TotalRow = resultSearch.Count;
+            string pagination = PageHelper.CreateLink(objPage.CurrentPage, objPage.TotalRow, objPage.PageSize, true);
+            var jsonResult = new
+            {
+                DataTable = resultSearch,
+                TotalRow = objPage.TotalRow,
+                Pagination = pagination
+            };
+            return Json(jsonResult, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
