18dd741b17234bc1b66c308e6d79a5315a17d866
alanpebr <33680403+alanpebr@users.noreply.github.com>
2017-12-08 11:47:05 +0700

    Add UserData.cs

---
+++ ITI.Survey.Web/ITI.Survey.Web.UI/Models/UserData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITI.Survey.Web.UI.Models
+{
+    public class UserData
+    {
+        public string HEID { get; set; }
+        public string OPID { get; set; }
+        public UserData()
+        {
+            this.HEID = string.Empty;
+            this.OPID = string.Empty;
+        }
+    }
+}
