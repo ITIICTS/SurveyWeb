93fbca0dcf75aaa557d40d27c503523bba43e4a8
alanpebr <33680403+alanpebr@users.noreply.github.com>
2017-12-09 16:03:51 +0700

    publish profile

--- ITI.Survey.Web/ITI.Survey.Web.WebService/Properties/AssemblyInfo.cs
+++ ITI.Survey.Web/ITI.Survey.Web.WebService/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ITI.Survey.Web.WebService")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Web Service Survey")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ITI.Survey.Web.WebService")]
