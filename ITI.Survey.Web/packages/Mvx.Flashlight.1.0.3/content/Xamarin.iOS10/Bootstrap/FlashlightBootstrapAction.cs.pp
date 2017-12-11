5c3443d22694ebdad8bf55c0614ab27b466b931a
alanpebr <33680403+alanpebr@users.noreply.github.com>
2017-12-11 23:43:48 +0700

    Commit Package

---
+++ ITI.Survey.Web/packages/Mvx.Flashlight.1.0.3/content/Xamarin.iOS10/Bootstrap/FlashlightBootstrapAction.cs.pp
@@ -0,0 +1,7 @@
+using MvvmCross.Platform.Plugins;
+
+namespace $rootnamespace$.Bootstrap
+{
+    public class FlashlightPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<Flashlight.PluginLoader, Flashlight.iOS.Plugin> { }
+}
