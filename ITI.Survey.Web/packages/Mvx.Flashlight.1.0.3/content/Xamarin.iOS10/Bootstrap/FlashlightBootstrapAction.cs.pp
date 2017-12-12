663b34df6196bb375804ade54d074c569d84f3d7
alanpebr <33680403+alanpebr@users.noreply.github.com>
2017-12-12 16:11:33 +0700

    Delete file not use

--- ITI.Survey.Web/packages/Mvx.Flashlight.1.0.3/content/Xamarin.iOS10/Bootstrap/FlashlightBootstrapAction.cs.pp
+++
@@ -1,7 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace $rootnamespace$.Bootstrap
-{
-    public class FlashlightPluginBootstrap
-        : MvxLoaderPluginBootstrapAction<Flashlight.PluginLoader, Flashlight.iOS.Plugin> { }
-}
